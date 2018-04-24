01fece06eeb5a13f268383998e3b567571a57ff0
Yuki Hirai <yukihirai0505@gmail.com>
2018-04-24 12:12:25 +0900

    Add chrom-extension sample

---
+++ js/background.js
@@ -0,0 +1,14 @@
+var counter = 0;
+chrome.tabs.getAllInWindow( null, function( tabs ){
+  counter = tabs.length;
+  chrome.browserAction.setBadgeText({ text:String(counter) });
+});
+chrome.tabs.onCreated.addListener( function( tab ){
+  counter++;
+  chrome.browserAction.setBadgeText({ text: String(counter) });
+});
+chrome.tabs.onRemoved.addListener( function( tab ){
+  counter--;
+  chrome.browserAction.setBadgeText({ text: String(counter) });
+});
+chrome.browserAction.setTitle({ title: "hoge", tabId: 1 });
--- shell_scripts/zip_extension.sh
+++ shell_scripts/zip_extension.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+
 zip -r chrome-extension.zip *
